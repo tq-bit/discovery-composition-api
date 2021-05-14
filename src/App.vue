adc14852b075251340736ed0f1530436d9676823
Tobias Quante <t.quante@outlook.com>
2021-05-14 12:41:41 +0200

    ref: move todoList to component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,17 @@
 <template>
   <main class="app-wrapper">
-    <todo-input v-model="todoInputModel"></todo-input>
-    <todo-button @click="addTodo" label="Add to list"></todo-button>
     <todo-list :todos="todoState"></todo-list>
-    {{ todoCount }}
-    {{ todoState }}
   </main>
 </template>
 
 <script>
-import TodoInput from "@/components/TodoInput.vue";
-import TodoButton from "@/components/TodoButton.vue";
 import TodoList from "@/components/TodoList.vue";
 
-import useTodos from "@/use/useTodos.js";
 
 export default {
-  setup() {
-    const {
-      todoInputModel,
-      todoCount,
-      addTodo,
-      todoState,
-      removeTodo,
-    } = useTodos();
-    return { todoInputModel, todoCount, addTodo, todoState, removeTodo };
-  },
   components: {
-    TodoInput,
-    TodoButton,
-    TodoList,
-  },
+    TodoList
+  }
 };
 </script>
 
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,27 +1,38 @@
 <template>
-  <ul class="todo-list">
+  <todo-input v-model="todoInputModel"></todo-input>
+  <todo-button @click="addTodo" label="Add to list"></todo-button>
 
-  <todo-item
-    v-for="(todo, index) in todos"
-    :key="todo"
-    :item="todo"
-    :index="index"
-  ></todo-item>
+  <ul class="todo-list">
+    <todo-item
+      v-for="(todo, index) in todoState"
+      :key="todo"
+      :item="todo"
+      :index="index"
+    ></todo-item>
   </ul>
 </template>
 
 <script>
+import TodoInput from "@/components/TodoInput.vue";
+import TodoButton from "@/components/TodoButton.vue";
 import TodoItem from "./TodoItem.vue";
 
+import useTodos from "@/use/useTodos.js";
+
 export default {
-  setup() {},
-  props: {
-    todos: {
-      type: Array,
-      required: true,
-    },
+  setup() {
+    const {
+      todoInputModel,
+      todoCount,
+      addTodo,
+      todoState,
+      removeTodo,
+    } = useTodos();
+    return { todoInputModel, todoCount, addTodo, todoState, removeTodo };
   },
   components: {
+    TodoInput,
+    TodoButton,
     TodoItem,
   },
 };
